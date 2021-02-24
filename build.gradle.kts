ba0d42a88c974aab336b36873c795b6e51a41a18
Ebn Zhang <ebnbin@gmail.com>
2021-02-24 19:29:45 +0800

    Config

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+buildscript {
+    apply("https://raw.githubusercontent.com/ebnbin/gradle-extra/0.0.4/extra.gradle.kts")
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        val dependencyMap: Map<String, String> by rootProject.extra
+        fun dependency(id: String): String {
+            val version = dependencyMap[id].also {
+                requireNotNull(it)
+            }
+            return "$id:$version"
+        }
+        classpath(dependency("com.android.tools.build:gradle"))
+        classpath(dependency("org.jetbrains.kotlin:kotlin-gradle-plugin"))
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
---
+++ lib/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    `maven-publish`
+}
+
+android {
+    val versionMap: Map<String, String> by rootProject.extra
+    compileSdkVersion(versionMap.getValue("compileSdkVersion").toInt())
+    defaultConfig {
+        minSdkVersion(versionMap.getValue("minSdkVersion").toInt())
+        targetSdkVersion(versionMap.getValue("targetSdkVersion").toInt())
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    val dependencyMap: Map<String, String> by rootProject.extra
+    fun dependency(id: String): String {
+        val version = dependencyMap[id].also {
+            requireNotNull(it)
+        }
+        return "$id:$version"
+    }
+    implementation(dependency("androidx.core:core-ktx"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+            }
+        }
+    }
+}
