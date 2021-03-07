f5bd164f29fd915cd2ab31939b8e465a200c01c5
Ebn Zhang <ebnbin@gmail.com>
2021-03-07 16:07:49 +0800

    BindingAdapter.kt

--- lib/src/main/java/dev/ebnbin/ebui/BindingAdapter.kt
+++ lib/src/main/java/dev/ebnbin/ebui/BindingAdapter.kt
@@ -1,10 +1,21 @@
 package dev.ebnbin.ebui
 
 import android.view.View
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("visible")
 fun setVisible(view: View, isVisible: Boolean) {
     view.isVisible = isVisible
+}
+
+@BindingAdapter("android:src")
+fun setImageResource(view: ImageView, @DrawableRes drawableId: Int) {
+    if (drawableId == 0) {
+        view.setImageDrawable(null)
+    } else {
+        view.setImageResource(drawableId)
+    }
 }
